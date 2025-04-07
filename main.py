31b34b223bbbbbf459641ff1e6b886d1e1de4979
SyroXik <gernathjak@gmail.com>
2025-04-07 15:10:13 +0200

    zakladne premenne

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+import tkinter as tk
+import random
+
+# vytvorenie okna
+okno = tk.Tk()
+okno.title("Pexeso")
+
+# vytvorenie premenných
+karty = list("AABBCCDDEEFFGGHH")  # 8 párov
+random.shuffle(karty)
+tlacidla = []
+prevratene = []
+najdene = []
