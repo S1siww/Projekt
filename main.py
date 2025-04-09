860be91d1b7844cde78e278ef211b7b5ab56133f
SyroXik <gernathjak@gmail.com>
2025-04-09 08:34:51 +0200

    prerobenie do pygame, vytvorenie okna a zakl. parametre

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-import tkinter as tk
+import pygame
 import random
+import time
 
-# vytvorenie okna
-okno = tk.Tk()
-okno.title("Pexeso")
+pygame.init()
 
-# vytvorenie premenných
-karty = list("AABBCCDDEEFFGGHH")  # 8 párov
-random.shuffle(karty)
-tlacidla = []
-prevratene = []
-najdene = []
+width, heigh = 600, 6
+velkost_pola = 4
+velkost_karty = width // velkost_pola
+font = pygame.font.Font(None, 72)
+maly_font = pygame.font.Font(None, 36)
+
+biela = (255, 255, 255)
+cierna = (0, 0, 0)
+siva = (200, 200, 200)
+modra = (0, 0, 255)
+cervena = (255, 0, 0)
