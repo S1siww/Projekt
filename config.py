bccd2767f74d9709667b7994a0fb1f08d38236cf
S1siww <janscavnicky545@gmail.com>
2025-04-15 21:39:43 +0200

    pridanie farieb do configu v rgb

--- config.py
+++ config.py
@@ -1,1 +1,17 @@
+WIDTH = 600
+HEIGHT = 600
+VELKOST_POLA = 4
+VELKOST_KARTY = WIDTH // VELKOST_POLA
 
+# Farby
+BIELA = (255, 255, 255)
+CIERNA = (0, 0, 0)
+SIVA = (200, 200, 200)
+MODRA = (0, 0, 255)
+CERVENA = (255, 0, 0)
+
+# Písma
+import pygame
+pygame.font.init()
+FONT = pygame.font.Font(None, 72)
+MALY_FONT = pygame.font.Font(None, 36)
